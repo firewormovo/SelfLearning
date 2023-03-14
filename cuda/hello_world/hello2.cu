6f23e7209fca6d97df70d0b5111216a218665983
firewormovo <1096026395@qq.com>
2023-03-14 21:29:51 +0800

    删除历史记录

--- cuda/hello_world/hello2.cu
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-__global__ void hello_from_gpu() { printf("hello world from gpu\n"); }
-
-int main() {
-  hello_from_gpu<<<2, 3>>>();
-  cudaDeviceSynchronize();
-  return 0;
-}
